9d8f83a17aced09b0fd49c2dcc7205b7c3be596e
lianghwajou.gmail.com <lianghwajou.gmail.com>
2022-09-23 19:47:21 +0000

    Changed indentation to 4 spaces

--- app.js
+++ app.js
@@ -36,22 +36,22 @@ const bot = new Bot(zendesk, session);
 zendesk.bot = bot;
 
 if (config.useWebhook) {
-  app.post(config.botPath, bot.botHandler.bind(bot));
+    app.post(config.botPath, bot.botHandler.bind(bot));
 }
 
 // zendesk channel route
 app.get('/channel/manifest', (req, res)=>{
-  zendesk.manifest(res);
+    zendesk.manifest(res);
 });
 
 app.post('/channel/admin_ui', (req, res)=>{
-  zendesk.admin_ui(req, res);
+    zendesk.admin_ui(req, res);
 });
 
 //token, name, return_url
 app.post('/channel/admin_ui_2', async (req, res)=>{
-  // setupBot(req, config.useWebhook);
-  zendesk.admin_ui_2(req, res);
+    // setupBot(req, config.useWebhook);
+    zendesk.admin_ui_2(req, res);
 });
 
 // function resetBot(req, useWebhook) {
@@ -70,37 +70,37 @@ app.post('/channel/admin_ui_2', async (req, res)=>{
 // }
 
 app.post('/channel/pull', (req,res)=>{
-  // resetBot(req, config.useWebhook);
-  zendesk.pull(req, res);
+    // resetBot(req, config.useWebhook);
+    zendesk.pull(req, res);
 }); 
 
 
 app.post('/channel/channelback', (req, res)=>{
-  // resetBot(req, config.useWebhook);
-  zendesk.channelback(req, res);
+    // resetBot(req, config.useWebhook);
+    zendesk.channelback(req, res);
 });
 
 // utility route
 app.post('/channel/event_callback', (req, res)=>{
-  console.log("Event callback");
-  console.log(req.body);
-  res.sendStatus(200);
+    console.log("Event callback");
+    console.log(req.body);
+    res.sendStatus(200);
 });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
