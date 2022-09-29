f74dca09a9b9d97916ae798a5956b8e66cfbc827
lianghwajou.gmail.com <lianghwajou.gmail.com>
2022-09-29 03:12:17 +0000

    Load questions from json.

--- survey.js
+++ survey.js
@@ -1,33 +1,16 @@
+const questions = require("./questions.json");
 const questionnaire = {
 	startMsg: "Start message",
 	postMsg: "Someone will provide feedback",
 	title: 1,
-	qns: [
-		{
-			id: "phone",
-			prompt: "What's your phone number?",
-			type: "text",
-			validation: /\d+/,
-			errorMsg: "Invalid phone number",
-			retry: 2,
-			form: 'user',
-			fieldId: "text_field_key",
-		},
-		{
-			name: "destination",
-			prompt: "What's your destination?",
-			type: "text",
-			validation: /(?:)/,
-			errorMsg: "Invalid destination",
-			retry: 2,
-			form: 'ticket',
-			fieldId: "text_field",
-		}
-	]
+	qns: undefined
 };
 
 class Survey{
 	getQre () {
+		if (!questionnaire.qns) {
+			questionnaire.qns = this.processQuestions();
+		}
 		return questionnaire;
 	}
 
@@ -36,6 +19,18 @@ class Survey{
 			return questionnaire.qns.length - 1;
 		} else {
 			return questionnaire.title;
+		}
+	}
+
+	processQuestions () {
+		try {
+			let questions = require("./questions.json");
+			for (let question of questions) {
+				question.validation = new RegExp(question.validation);
+			}
+			return questions;
+		} catch (e) {
+			console.error(e);
 		}
 	}
 }
